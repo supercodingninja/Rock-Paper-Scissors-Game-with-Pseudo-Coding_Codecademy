9a2862c60ae9d831bb5b6349c7dfae42356d9157
Frederick D. Thomas, Jr.; Rev., USMC OEF/OIF I/OIF II Vet <frederickdthomasjr@gmail.com>
2017-05-31 18:13:03 -0700

    Update IXdH-0.js

--- IXdH-0.js
+++ IXdH-0.js
@@ -64,16 +64,115 @@ function compare (choice1, choice2) {
     
     if (choice1 > choice2) {
         // Show "Outcome" //
-        return ("You Win");
+       // return ("You Win"); //
     }
     
     else if (choice1 < choice2) {
         // Show "Outcome" //
-        return ("You Lost; but try again.");
+       // return ("You Lost; but try again."); //
     }
     
     else if (choice1 == choice2) {
         // Show "Outcome" //
         return ("The result is a tie!");
+    }
+        
+    /*Now, I must consider the other scenarios. I need to break the problem down a little. What if choice1 is "rock"? Given choice1 is "rock",
+
+a. if choice2 === "scissors", then "rock" wins.
+b. if choice2 === "paper", then "paper" wins.
+
+How do I structure this? It's a bit different from what I have already seen, in my earlier code. I will first, need an if statement; and then another code inside that if statement; which will be... another if statement!*/
+    else if (choice1 === "Rock") {
+        
+        if (choice2 === "Scissors") {
+            // Show "Outcome" //
+            return ("Rock Wins");
+        }
+        
+        else if (choice2 === "Paper") {
+            // Show "Outcome" //
+            return ("Paper Wins");
+        }
+    }
+    
+    else if (choice1 === "Paper") {
+        
+        if (choice2 === "Rock") {
+            // Show "Outcome" //
+            return ("Paper Wins");
+        }
+        
+        else if (choice2 === "Scissors") {
+            // Show "Outcome" //
+            return ("Scissors Wins");
+        }
+    }
+    
+    else if (choice1 === "Scissors") {
+        
+        if (choice2 === "Rock") {
+            // Show "Outcome" //
+            return ("Rock Wins");
+        }
+        
+        else if (choice2 === "Scissors") {
+            // Show "Outcome" //
+            return ("Scissors Wins");
+        }
+    }
+    
+    // What if a user makes an inappropriate choice like 'dog'? How can I extend the function to handle that? //
+    else if (choice1 != "Rock", "Paper", "Scissors") {
+            // Show "Outcome" //
+            return ("Really?!  Rock, Paper, or Scissors: make a choice of these three; or you automatically lose.");
+    }
+        if (choice1 != "Rock", "Paper", "Scissors") {
+            // Show "Outcome" //
+            return ("Game Over: you don't want to play, your lost.");
+        }
+        
+        else if (choice2 === "Scissors") {
+            // Show "Outcome" //
+            return ("Scissors Wins");
+        }
+        
+        else if (choice1 === "Rock") {
+        
+            if (choice2 === "Scissors") {
+                // Show "Outcome" //
+                return ("Rock Wins");
+            }
+        
+            else if (choice2 === "Paper") {
+                // Show "Outcome" //
+                return ("Paper Wins");
+            }
+        }
+    
+    else if (choice1 === "Paper") {
+        
+        if (choice2 === "Rock") {
+            // Show "Outcome" //
+            return ("Paper Wins");
+        }
+        
+        else if (choice2 === "Scissors") {
+            // Show "Outcome" //
+            return ("Scissors Wins");
+        }
+    }
+    
+    else if (choice1 === "Scissors") {
+        
+        if (choice2 === "Rock") {
+            // Show "Outcome" //
+            return ("Rock Wins");
+        }
+        
+        else if (choice2 === "Scissors") {
+            // Show "Outcome" //
+            return ("Scissors Wins");
+        }
     }
 }
